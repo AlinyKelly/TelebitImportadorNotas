fde7ef62e0811e85d7c3c6a2ef73e35e9e1ec4a7
Aliny Kelly <alinykellyfs@gmail.com>
2024-06-25 13:13:52 -0300

    Commit 7 - Construido importador de Folha de serviço

--- src/botoes/ImportarFS.kt
+++ src/botoes/ImportarFS.kt
@@ -3,102 +3,182 @@ package botoes
 import br.com.sankhya.extensions.actionbutton.AcaoRotinaJava
 import br.com.sankhya.extensions.actionbutton.ContextoAcao
 import br.com.sankhya.jape.core.JapeSession
+import br.com.sankhya.jape.vo.DynamicVO
 import br.com.sankhya.jape.wrapper.JapeFactory
 import br.com.sankhya.modelcore.MGEModelException
-import com.sankhya.ce.jape.JapeHelper
-import utilitarios.getPropFromJSON
-import utilitarios.post
+import br.com.sankhya.ws.ServiceContext
+import org.apache.commons.io.FileUtils
+import java.io.BufferedReader
+import java.io.File
+import java.io.FileReader
+import java.io.IOException
 import java.math.BigDecimal
 import java.sql.Timestamp
+import java.text.DateFormat
 import java.text.SimpleDateFormat
 import java.util.*
-import java.util.stream.Collectors
-import kotlin.collections.ArrayList
+import java.util.regex.Matcher
+import java.util.regex.Pattern
 
 class ImportarFS : AcaoRotinaJava {
-    override fun doAction(contextoAcao: ContextoAcao?) {
-        val notas: MutableList<BigDecimal?> = ArrayList()
-        val nunotaPOcab: MutableList<BigDecimal> = ArrayList()
-        val linhas = contextoAcao!!.linhas
-        var nunotaPedido: BigDecimal? = null
+    @Throws(MGEModelException::class, IOException::class)
+    override fun doAction(contextoAcao: ContextoAcao) {
+        var hnd: JapeSession.SessionHandle? = null
 
-        for (linha in linhas) {
-            val codImportacao = linha.getCampo("CODIMPPO")
+        var ultimaLinhaJson: LinhaJson? = null
 
-            val pos = JapeHelper.getVOs(
-                "AD_IMPORTPOITE",
-                "CODIMPPO = $codImportacao"
-            )
-            for (po in pos) {
-                val codimpIte = po.asBigDecimalOrZero("CODIMPPO")
-                val nunota = po.asBigDecimalOrZero("NUNOTA")
-                val tipoOperacao = contextoAcao.getParametroSistema("TOPPO")
-                val dtFaturamento = po.asTimestamp("EMISSAOPO")
+        val linhasSelecionadas = contextoAcao.linhas
 
-                if (!nunotaPOcab.contains(nunota)) {
-                    val json = """  {
-                                     "serviceName":"SelecaoDocumentoSP.faturar",
-                                     "requestBody":{
-                                        "notas":{
-                                           "codTipOper":$tipoOperacao,
-                                           "dtFaturamento":"$dtFaturamento",
-                                           "tipoFaturamento":"FaturamentoNormal",
-                                           "dataValidada":true,
-                                           "notasComMoeda":{
-                                              
-                                           },
-                                           "nota":[
-                                              {
-                                                 "${'$'}": $nunota
-                                              }
-                                           ],
-                                           "codLocalDestino":"",
-                                           "faturarTodosItens":true,
-                                           "umaNotaParaCada":"false",
-                                           "ehWizardFaturamento":true,
-                                           "dtFixaVenc":"",
-                                           "ehPedidoWeb":false,
-                                           "nfeDevolucaoViaRecusa":false
-                                        }
-                                     }
-                                  }""".trimIndent()
+        try {
+            for (linha in linhasSelecionadas) {
+                var count = 0
 
-                    val (postbody) = post("https://api.sankhya.com.br/gateway/v1/mgecom/service.sbr?serviceName=SelecaoDocumentoSP.faturar&outputType=json", json)
-                    val status = getPropFromJSON("status", postbody)
-                    val statusMessage = getPropFromJSON("statusMessage", postbody)
+                val codimportacao = linha.getCampo("CODFS") as BigDecimal?
 
-                    nunotaPOcab.add(nunota)
+                val data = linha.getCampo("ARQUIVO") as ByteArray?
+                val ctx = ServiceContext.getCurrent()
+                val file = File(ctx.tempFolder, "ARQUIVOITEFS" + System.currentTimeMillis())
+                FileUtils.writeByteArrayToFile(file, data)
+
+                hnd = JapeSession.open()
+
+                BufferedReader(FileReader(file)).use { br ->
+                    var line = br.readLine()
+
+                    while (line != null) {
+                        if (count == 0) {
+                            count++
+                            line = br.readLine()
+                            continue
+                        }
+                        count++
+
+                        if (line.contains("__end_fileinformation__")) {
+                            // The substituted value will be contained in the result variable
+                            line = getReplaceFileInfo(line);
+                        }
+
+                        val json = trataLinha(line)
+                        ultimaLinhaJson = json
+
+                        val novaLinhaIte = contextoAcao.novaLinha("AD_IMPORTPOITE")
+                        novaLinhaIte.setCampo("CODIMPPO", codimportacao)
+                        novaLinhaIte.setCampo("NUNOTA", json.nroUnicoPO.trim())
+                        novaLinhaIte.setCampo("NUNOTAFS", json.nroUnicoFS.trim())
+                        novaLinhaIte.setCampo("SEQUENCIA", json.seqItem.trim())
+                        novaLinhaIte.setCampo("ITEMBOQ", json.itemBOQ.trim())
+                        novaLinhaIte.setCampo("IDANDAMENTO", json.idandamento.trim())
+                        novaLinhaIte.setCampo("IDATIVIDADE", json.idatividade.trim())
+                        novaLinhaIte.setCampo("NROFS", json.fs.trim())
+                        novaLinhaIte.setCampo("QTDFS", json.qtdFs.trim())
+                        novaLinhaIte.setCampo("EMISSAOFS", formatarDataString(json.emissaoFS.trim()))
+                        novaLinhaIte.setCampo("STATUSFS", json.statusFS.trim())
+                        novaLinhaIte.setCampo("DADOSADICIONAIS", json.dadosAdicionais.trim())
+                        novaLinhaIte.save()
+
+                        line = br.readLine()
+
+                    }
+
                 }
 
             }
+        } catch (e: Exception) {
+            throw MGEModelException("$e $ultimaLinhaJson ")
+        } finally {
+            JapeSession.close(hnd)
         }
+    }
 
-        contextoAcao!!.setMensagemRetorno(
-            "Pedidos criados com sucesso! " + notas.stream().map { obj: BigDecimal? -> obj.toString() }
-                .collect(Collectors.joining(",")))
+    private fun getReplaceFileInfo(line: String): String {
+        val regex = "__start_fileinformation__.*__end_fileinformation__"
+        val subst = ""
+
+        val pattern: Pattern = Pattern.compile(regex, Pattern.MULTILINE)
+        val matcher: Matcher = pattern.matcher(line)
+
+        return matcher.replaceAll(subst)
+    }
+
+    private fun trataLinha(linha: String): LinhaJson {
+        var cells = if (linha.contains(";")) linha.split(";(?=([^\"]*\"[^\"]*\")*[^\"]*\$)".toRegex()).toTypedArray()
+        else linha.split(",(?=([^\"]*\"[^\"]*\")*[^\"]*\$)".toRegex()).toTypedArray()
+
+        cells = cells.filter { predicate ->
+            if (predicate.isEmpty())
+                return@filter false
+            return@filter true
+        }.toTypedArray() // Remove linhas vazias
+        val ret = if (cells.isNotEmpty()) LinhaJson(
+            cells[0],
+            cells[1],
+            cells[2],
+            cells[3],
+            cells[4],
+            cells[5],
+            cells[6],
+            cells[7],
+            cells[8],
+            cells[9],
+            cells[10]
+        ) else
+            null
+
+        if (ret == null) {
+            throw Exception("Erro ao processar a linha: $linha")
+        }
+        return ret
 
     }
 
-    private fun inserirNunotaImp(
-        intancia: String,
-        codImportacao: Any,
-        codigo: BigDecimal,
-        nunota: BigDecimal?,
-        sequencia: BigDecimal
-    ) {
+    @Throws(MGEModelException::class)
+    fun retornaVO(instancia: String?, where: String?): DynamicVO? {
+        var dynamicVo: DynamicVO? = null
         var hnd: JapeSession.SessionHandle? = null
-
         try {
             hnd = JapeSession.open()
-            JapeFactory.dao(intancia).prepareToUpdateByPK(codImportacao, codigo)
-                .set("NUNOTA", nunota)
-                .set("SEQUENCIA", sequencia)
-                .update()
-        } catch (e: Exception) {
+            val instanciaDAO = JapeFactory.dao(instancia)
+            dynamicVo = instanciaDAO.findOne(where)
+        } catch (e: java.lang.Exception) {
             MGEModelException.throwMe(e)
         } finally {
             JapeSession.close(hnd)
         }
+        return dynamicVo
+    }
+
+    /**
+     * Converte um valor (String) com separador de milhares para [BigDecimal]
+     * @author Aliny Sousa
+     * @param str  Texto a ser convertido
+     * @return [BigDecimal]
+     */
+    fun converterValorMonetario(valorMonetario: String): BigDecimal {
+        val valorNumerico = valorMonetario.replace("\"", "").replace(".", "").replace(",", ".")
+        return BigDecimal(valorNumerico)
+    }
+
+    /**
+     * Converte uma data dd/mm/yyyy ou dd-mm-yyyy em timestampb
+     * @author Luis Ricardo Alves Santos
+     * @param strDate  Texto a ser convertido
+     * @return [Timestamp]
+     */
+    fun stringToTimeStamp(strDate: String): Timestamp? {
+        try {
+            val formatter: DateFormat = SimpleDateFormat("MM/dd/yyyy")
+            val date: Date = formatter.parse(strDate)
+            return Timestamp(date.time)
+        } catch (e: Exception) {
+            return null
+        }
+    }
+
+    /**
+     * Retorna a data atual
+     */
+    fun getDhAtual(): Timestamp {
+        return Timestamp(System.currentTimeMillis())
     }
 
     fun formatarDataString(originalDateStr: String): Timestamp? {
@@ -122,4 +202,18 @@ class ImportarFS : AcaoRotinaJava {
         }
 
     }
+
+    data class LinhaJson(
+        val nroUnicoPO: String,
+        val nroUnicoFS: String,
+        val seqItem: String,
+        val itemBOQ: String,
+        val idandamento: String,
+        val idatividade: String,
+        val fs: String,
+        val qtdFs: String,
+        val emissaoFS: String,
+        val statusFS: String,
+        val dadosAdicionais: String
+    )
 }
