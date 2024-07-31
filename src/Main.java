a3d6e6c53646895a4e22f2a9700c543276ce0c00
Aliny Kelly <alinykellyfs@gmail.com>
2024-07-31 11:32:21 -0300

    Commit 23 - Desenvolvimento da tela de Gestão de Projetos.

--- src/Main.java
+++ src/Main.java
@@ -1,65 +1,11 @@
+import java.math.BigDecimal;
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-//        {
-//            "serviceName": "SelecaoDocumentoSP.faturar",
-//                "requestBody": {
-//            "notas": {
-//                "codTipOper": $top,
-//                        "dtFaturamento": "$data",
-//                        "serie": "1",
-//                        "dtSaida": "",
-//                        "hrSaida": "",
-//                        "tipoFaturamento": "FaturamentoNormal",
-//                        "dataValidada": true,
-//                        "notasComMoeda": {},
-//                "nota": [
-//                { "NUNOTA": "$nota", "itens": { "item": [{ "QTDFAT": 1, "$": $sequencia }] } }
-//      ],
-//                "codLocalDestino": "",
-//                        "conta2": 0,
-//                        "faturarTodosItens": false,
-//                        "umaNotaParaCada": "false",
-//                        "ehWizardFaturamento": true,
-//                        "dtFixaVenc": "",
-//                        "ehPedidoWeb": false,
-//                        "nfeDevolucaoViaRecusa": false,
-//                        "isFaturamentoDanfeSeguranca": false
-//            },
-//            "clientEventList": {
-//                "clientEvent": [
-//                { "$": "br.com.sankhya.actionbutton.clientconfirm" },
-//                { "$": "br.com.sankhya.mgecom.enviar.recebimento.wms.sncm" },
-//                { "$": "comercial.status.nfe.situacao.diferente" },
-//                { "$": "br.com.sankhya.mgecom.compra.SolicitacaoComprador" },
-//                { "$": "br.com.sankhya.mgecom.expedicao.SolicitarUsuarioConferente" },
-//                { "$": "br.com.sankhya.mgecom.nota.adicional.SolicitarUsuarioGerente" },
-//                { "$": "br.com.sankhya.mgecom.cancelamento.nfeAcimaTolerancia" },
-//                { "$": "br.com.sankhya.mgecom.cancelamento.processo.wms.andamento" },
-//                { "$": "br.com.sankhya.mgecom.msg.nao.possui.itens.pendentes" },
-//                { "$": "br.com.sankhya.mgecomercial.event.baixaPortal" },
-//                { "$": "br.com.sankhya.mgecom.valida.ChaveNFeCompraTerceiros" },
-//                { "$": "br.com.sankhya.mgewms.expedicao.validarPedidos" },
-//                { "$": "br.com.sankhya.mgecom.gera.lote.xmlRejeitado" },
-//                { "$": "br.com.sankhya.comercial.solicitaContingencia" },
-//                { "$": "br.com.sankhya.mgecom.cancelamento.notas.remessa" },
-//                { "$": "br.com.sankhya.mgecomercial.event.compensacao.credito.debito" },
-//                {
-//                    "$": "br.com.sankhya.modelcore.comercial.cancela.nota.devolucao.wms"
-//                },
-//                { "$": "br.com.sankhya.mgewms.expedicao.selecaoDocas" },
-//                { "$": "br.com.sankhya.mgewms.expedicao.cortePedidos" },
-//                {
-//                    "$": "br.com.sankhya.modelcore.comercial.cancela.nfce.baixa.caixa.fechado"
-//                },
-//                { "$": "br.com.utiliza.dtneg.servidor" },
-//                {
-//                    "$": "br.com.sankhya.mgecomercial.event.estoque.insuficiente.produto"
-//                }
-//      ]
-//    }
-//  }
-//        }
-}
+
+            System.out.print("col_VLRPRODUCAO");
+    }
 }
